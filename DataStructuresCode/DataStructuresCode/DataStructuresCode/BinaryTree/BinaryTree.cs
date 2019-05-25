c3e2d7e282338ce49cf2b71a2f47a265dd3a118a
Patrick <Patricknnoonan@gmail.com>
2019-05-25 09:04:31 -0500

    repo init

---
+++ DataStructuresCode/DataStructuresCode/DataStructuresCode/BinaryTree/BinaryTree.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresCode.BinaryTree
+{
+    class BinaryTree
+    {
+        //has
+        public Node root;
+        public Node current;
+        public Node parent;
+        public Node leaf;
+
+        //constructor
+        public BinaryTree()
+        {
+            root = null;
+        }
+
+        //does this
+        public void Add(int num)
+        {
+            if (root == null)
+            {
+                root = new Node(num);
+            }
+            else
+            {
+
+
+
+
+
+
+
+            }
+        }
+        public void Search(int num)
+        {
+
+        }
+    }
+}
---
+++ DataStructuresCode/DataStructuresCode/DataStructuresCode/BinaryTree/Node.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructuresCode.BinaryTree
+{
+    class Node
+    {
+        //has this
+        public int info;
+        public Node left;
+        public Node right;
+
+        //constructor
+        public Node(int num)
+        {
+            info = num;
+            left = null;
+            right = null;
+        }
+        //does this
+        public void NodeInfo()
+        {
+            Console.WriteLine(info);
+            Console.ReadLine();
+        }
+    }
+}
